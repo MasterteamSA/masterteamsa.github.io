c7fe640ac93d45e85a488c78e30260415912d6b3
Mohammed Shammout <fdsmohammed@gmail.com>
2022-06-16 15:26:52 +0300

    update branding and add Development Environment page

---
+++ docs/workflow/development-environment.md
@@ -0,0 +1,22 @@
+---
+sidebar_position: 2
+---
+
+# Development Environment
+
+## Software Versions
+
+|          | Version |
+|----------|---------|
+| Node     | 16.x    |
+| Yarn     | 1.x     | 
+
+## Packages
+
+| Package       | Version | Notes         |   
+|---------------|---------|---------------|
+| react         | 17.x    |               |
+| react-scripts | 5.x     |               |
+| cross-env     | 7.x     |               |
+| redux-toolkit | 1.x     |               |
+| sass          | 1.x     | not node-sass | 
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "img/mw-square-logo.png",
   organizationName: "MasterteamSA", // Usually your GitHub org/user name.
   projectName: "masterteamsa.github.io", // Usually your repo name.
   themeConfig: {
@@ -14,7 +14,7 @@ module.exports = {
       title: "MT Guideline",
       logo: {
         alt: "My Site Logo",
-        src: "img/logo.svg",
+        src: "img/mw-square-logo.png",
       },
       items: [
         {
